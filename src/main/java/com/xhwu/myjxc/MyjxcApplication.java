20abea33fbce427e90b241972bb6e174f7ccaaad
wuxh <215413095@qq.com>
2019-08-06 20:13:12 +0800

    Initial commit

---
+++ src/main/java/com/xhwu/myjxc/MyjxcApplication.java
@@ -0,0 +1,13 @@
+package com.xhwu.myjxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjxcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyjxcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhwu/myjxc/MyjxcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xhwu.myjxc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyjxcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
